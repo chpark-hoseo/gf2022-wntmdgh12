5fadcbda73e3d36144c3c049629e3a2679d899b3
wntmdgh12 <20203311@vision.hoseo.edu>
2022-09-23 13:22:33 +0900

    추가실습
    1.바꿔서 수정하기
    2. 랜덤하기 변경하는 코드추가

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿// SDL 
 //
 // written by changhoonpark@gmail.com
@@ -205,3 +206,73 @@ void render()
 
 	SDL_RenderPresent(g_pRenderer);
 }
+=======
+﻿#include <SDL2/SDL.h>
+#include <cstdlib>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+    {
+
+        g_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, height, width, flags);
+
+        if (g_pWindow != 0) 
+        {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
+
+    }
+    else 
+    {
+        return false;
+    }
+
+    SDL_SetRenderDrawColor(g_pRenderer, rand() % 255, rand() % 255, rand()%255, 255);
+
+    return true;
+}
+
+void render()
+{
+    SDL_RenderClear(g_pRenderer);
+
+    // 그리기 수행  
+
+    SDL_RenderPresent(g_pRenderer);
+    g_bRunning = false;
+   
+}
+
+
+int main(int argc, char* argv[])
+{
+    if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 640, 480,
+        SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1; // something's wrong
+    }
+
+    while (g_bRunning)
+    {
+        // handle input - update - render
+        render();
+    }
+    SDL_Delay(2000);
+    SDL_Quit();
+    return 0;
+}
+
+
+   
+>>>>>>> Stashed changes
