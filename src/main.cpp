a899afea039e06d2596e4f4d43fcd886d27dee62
wntmdgh12 <20203311@vision.hoseo.edu>
2022-09-23 12:52:34 +0900

    추가 실습
    1024x768만들기
    윈도우 이름 Game FrameWork로 바꾸기

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char* args[])
 
     if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
 
-        g_pWindow = SDL_CreateWindow("Setting up SDL", //창 위에 이름
+        g_pWindow = SDL_CreateWindow("Game FrameWork", //윈도우 이름
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
-            640, 480, //창 크기
+            1024, 768, //윈도우 크기
             SDL_WINDOW_SHOWN);
 
         if (g_pWindow != 0) {
@@ -24,11 +24,11 @@ int main(int argc, char* args[])
     }
 
     SDL_SetRenderDrawColor(
-        g_pRenderer, 0, 0, 255, 255); //색상 rgb
-    SDL_RenderClear(g_pRenderer);
- //   SDL_RenderPresent(g_pRenderer);
+        g_pRenderer, 0, 0,0, 255); //색상 rgb
+    SDL_RenderClear(g_pRenderer); //화면 지우기
+    SDL_RenderPresent(g_pRenderer);
 
-    SDL_Delay(5000); //5초 뒤에 종료
+    SDL_Delay(5000); //10초 뒤에 종료
     SDL_Quit(); //종료
 
     return 0;
