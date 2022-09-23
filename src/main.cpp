5737991c43f00c11a8939283e6e0a43ff1c68512
wntmdgh12 <20203311@vision.hoseo.edu>
2022-09-23 12:49:51 +0900

    추가 실습

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@ int main(int argc, char* args[])
 
     if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
 
-        g_pWindow = SDL_CreateWindow("Setting up SDL",
+        g_pWindow = SDL_CreateWindow("Setting up SDL", //창 위에 이름
             SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED, 640, 480,
+            SDL_WINDOWPOS_CENTERED,
+            640, 480, //창 크기
             SDL_WINDOW_SHOWN);
 
         if (g_pWindow != 0) {
             g_pRenderer
-                = SDL_CreateRenderer(g_pWindow, -1, 0);
+                = SDL_CreateRenderer(g_pWindow, -1, 0); 
         }
     }
     else {
@@ -23,12 +24,12 @@ int main(int argc, char* args[])
     }
 
     SDL_SetRenderDrawColor(
-        g_pRenderer, 0, 0, 0, 255);
+        g_pRenderer, 0, 0, 255, 255); //색상 rgb
     SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
+ //   SDL_RenderPresent(g_pRenderer);
 
-    SDL_Delay(5000);
-    SDL_Quit();
+    SDL_Delay(5000); //5초 뒤에 종료
+    SDL_Quit(); //종료
 
     return 0;
 }
