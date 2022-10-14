d097fea306cdb57e95cd0de019d2bdd2136a8361
wntmdgh12 <20203311@game.hoseo.edu>
2022-10-14 13:46:50 +0900

    Game 클래스화
    ㄴㅁㄴㅇㄹ

---
+++ src/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "main.h"
+
+class Game
+{
+public:
+	Game() { }
+	~Game() { }
+
+	bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
+	void render();
+	void update();
+	bool running();
+	void handleEvents();
+	void clean();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+};
