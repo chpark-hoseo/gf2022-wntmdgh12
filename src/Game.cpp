795083345fc78621b6ef57005924a911c786fe29
wntmdgh12 <20203311@vision.hoseo.edu>
2022-12-02 13:37:44 +0900

    원본상자,대상상자_실습
    원본상자,대상상자_실습 입니다

---
+++ src/Game.cpp
@@ -0,0 +1,93 @@
+#include "Game.h"
+
+bool Game::init(const char* title, int xpos, int ypos, int height, int width, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        m_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, width, height, flags);
+        if (m_pWindow != 0) {
+            m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+
+            if (m_pRenderer != 0) {
+
+                m_destinationRectangle.w = m_sourceRectangle.w = 200;//너비
+                m_destinationRectangle.h = m_sourceRectangle.h = 100;//높이
+
+
+
+
+
+                m_destinationRectangle.x = 100;
+                m_destinationRectangle.y = 100;
+
+                SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+
+                SDL_QueryTexture(m_pTexture, NULL, NULL,
+                    &m_sourceRectangle.w, &m_sourceRectangle.h);
+                m_sourceRectangle.x = 0;
+                m_sourceRectangle.y = 0;
+
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+
+
+
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+            }
+            else {
+                return false; // 랜더러 생성 실패
+            }
+        }
+        else {
+            return false; // 윈도우 생설 실패 l
+        }
+    }
+    else {
+        return false; // SDL 초기화 실패
+    }
+
+
+    SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+    SDL_FreeSurface(pTempSurface);
+    m_bRunning = true;
+    return true;
+}
+void Game::update()
+{
+
+    // 게임 데이터 갱신 
+}
+void Game::render()
+{
+
+    SDL_RenderClear(m_pRenderer);
+    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+    SDL_RenderPresent(m_pRenderer);
+}
+bool Game::running()
+{
+    return m_bRunning;
+}
+void Game::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+        default:
+            break;
+        }
+    }
+}
+void Game::clean()
+{
+    SDL_DestroyWindow(m_pWindow);
+    SDL_DestroyRenderer(m_pRenderer);
+    SDL_Quit();
+}
