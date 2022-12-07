fd738b686011d71f89c0073bf61708b26f1c75ef
wntmdgh12 <96272223+wntmdgh12@users.noreply.github.com>
2022-12-07 09:22:19 +0900

    SDL_Image
    SDL_Image입니다

---
+++ src/Game.cpp
@@ -0,0 +1,94 @@
+#include "Game.h"
+#include <SDL2/SDL_image.h>
+
+
+
+bool Game::init(const char* title, int xpos, int ypos, int height, int width, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        m_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, width, height, flags);
+        if (m_pWindow != 0) {
+            m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+
+            if (m_pRenderer != 0) {
+
+                m_destinationRectangle.w = m_sourceRectangle.w = 128;//너비
+                m_destinationRectangle.h = m_sourceRectangle.h = 82;//높이
+
+
+                m_destinationRectangle.x = m_sourceRectangle.x = 0;
+                m_destinationRectangle.y = m_sourceRectangle.y = 0;
+
+                SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+
+                SDL_QueryTexture(m_pTexture, NULL, NULL,
+                    &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+
+
+
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+            }
+            else {
+                return false; // 랜더러 생성 실패
+            }
+        }
+        else {
+            return false; // 윈도우 생설 실패 l
+        }
+    }
+    else {
+        return false; // SDL 초기화 실패
+    }
+
+    //SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
+    SDL_Surface* pTempSurface = IMG_Load("Assets/animate-alpha.png");
+  
+    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+    SDL_FreeSurface(pTempSurface);
+    m_bRunning = true;
+    return true;
+}
+void Game::update()
+{
+    m_sourceRectangle.x = 128 * ((SDL_GetTicks() / 100) % 6);
+    // 게임 데이터 갱신 
+}
+void Game::render()
+{
+
+    SDL_RenderClear(m_pRenderer);
+    SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255); // 붉은색 배경
+    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+    SDL_RenderPresent(m_pRenderer);
+}
+bool Game::running()
+{
+    return m_bRunning;
+}
+void Game::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+        default:
+            break;
+        }
+    }
+}
+void Game::clean()
+{
+    SDL_DestroyWindow(m_pWindow);
+    SDL_DestroyRenderer(m_pRenderer);
+    SDL_Quit();
+}
